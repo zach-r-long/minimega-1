6db293a3b9ddbc3b1a648ebbfc5ef3349a7550de
Bryan T. Richardson <bryan@activeshadow.com>
2020-12-18 13:23:47 -0700

    Debian package updates to improve build env and include phenix

---
+++ docker-build.bash
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+
+which docker &> /dev/null
+
+if (( $? )); then
+  echo "Docker must be installed (and in your PATH) to use this build script. Exiting."
+  exit 1
+fi
+
+
+USER_UID=$(id -u)
+USERNAME=builder
+
+
+docker build -t minimega:builder -f - . <<EOF
+FROM golang:1.12
+
+RUN groupadd --gid $USER_UID $USERNAME \
+  && useradd -s /bin/bash --uid $USER_UID --gid $USER_UID -m $USERNAME
+
+RUN apt update && apt install -y libpcap-dev
+EOF
+
+
+echo BUILDING MINIMEGA...
+
+docker run -it --rm -v $(pwd):/minimega -w /minimega -u $USERNAME minimega:builder ./clean.bash
+docker run -it --rm -v $(pwd):/minimega -w /minimega -u $USERNAME minimega:builder ./build.bash
+docker run -it --rm -v $(pwd):/minimega -w /minimega -u $USERNAME minimega:builder ./doc.bash
+
+bin/pyapigen bin/minimega
+
+echo DONE BUILDING MINIMEGA
--- packaging/debian/build.bash
+++ packaging/debian/build.bash
@@ -1,32 +1,60 @@
-#!/bin/bash -e
+#!/bin/bash
+
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MM=$SCRIPT_DIR/../..
+PHENIX=$MM/phenix
 
-MM=$SCRIPT_DIR/../../
 
-echo BUILDING MINIMEGA...
-(cd $MM && ./build.bash && ./doc.bash)
-echo DONE BUILDING
+which docker &> /dev/null
+DOCKER=$?
+
+if (( $DOCKER == 0 )); then
+  echo "Docker detected, so Docker will be used to build minimega and phenix."
+
+  (cd $MM && $MM/docker-build.bash)
+  (cd $PHENIX && $PHENIX/docker-build.sh -d)
+else
+  echo "Docker not detected, so minimega will be built but phenix will not be."
+
+  (cd $MM && $MM/build.bash && $MM/doc.bash)
+fi
+
 
 echo COPYING FILES...
 
 DST=$SCRIPT_DIR/minimega/opt/minimega
+
 mkdir -p $DST
+
 cp -r $MM/bin $DST/
 cp -r $MM/doc $DST/
 cp -r $MM/lib $DST/
+
+if (( $DOCKER == 0 )); then
+  cp -r $PHENIX/bin $DST/
+fi
+
 mkdir -p $DST/misc
-cp -r $MM/misc/daemon $DST/misc/
+
+cp -r $MM/misc/daemon           $DST/misc/
+cp -r $MM/misc/uminiccc         $DST/misc/
+cp -r $MM/misc/uminirouter      $DST/misc/
 cp -r $MM/misc/vmbetter_configs $DST/misc/
-cp -r $MM/misc/web $DST/misc/
+cp -r $MM/misc/web              $DST/misc/
 
 DOCS=$SCRIPT_DIR/minimega/usr/share/doc/minimega
+
 mkdir -p $DOCS
-cp $MM/LICENSE $DOCS/
-cp -r $MM/LICENSES $DOCS/
+
+cp $MM_DIR/LICENSE     $DOCS/
+cp -r $MM_DIR/LICENSES $DOCS/
 
 echo COPIED FILES
 
-echo BUILDING PACKAGE...
+
+echo BUILDING DEB PACKAGE...
+
 (cd $SCRIPT_DIR && fakeroot dpkg-deb -b minimega)
-echo DONE
+
+echo DONE BUILDING DEB PACKAGE
